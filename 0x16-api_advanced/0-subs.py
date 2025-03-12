fdba85c7e45a24356f702b37cff9321fce361142
Topazytee <peakybeetle@gmail.com>
2025-03-12 05:59:31 -0700

    .py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,28 +1,19 @@
+
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+Script contains the number_of_subscribers function
 """
-import json
-import urllib.request
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    req = urllib.request.Request(url, headers=headers)
-    try:
-        with urllib.request.urlopen(req) as response:
-            if response.status == 200:
-                print(response.headers)
-                data = json.loads(response.read().decode())
-                subscribers = data['data']['subscribers']
-                return subscribers
-            else:
-                return 0
-    except urllib.error.HTTPError as e:
-        print(f"HTTP Error: {e.code}, Reason: {e.reason}")
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    except urllib.error.URLError as e:
-        print(f"URL Error: {e.reason}")
-        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
+
